693e0ade9ef3d5412ba1434745c9a226eebe0702
echachad-ux <echachad@iu.edu>
2026-07-09 15:01:17 -0500

    Update Deliverable Week 2

--- Deliverable Week 2.py
+++ Deliverable Week 2.py
@@ -1,90 +1,22 @@
 import pandas as pd
-
-# Import CSV
-sold = pd.read_csv("/Users/eshaanchachad/Desktop/IDXExchange/CRMLSSold.csv")
-sold = sold[sold["PropertyType"] == "Residential"]
-
-print("Sold Report")
-
-nullcount = sold.isnull().sum()
-print("Count of null values")
-print(nullcount)
-
-#Print column types
-types = sold.dtypes
-print("Column types")
-print(types)
-
-# Percentage of null columns
-nullcolumns = sold.isnull().mean()*100
-print("Percent of null values within columns:")
-print(nullcolumns)
-
-# Flag columns with null values over 90%
-flaggedcols = sold.isnull().mean() >= 0.9
-flags = sold.columns[flaggedcols]
-print("Flagged columns (over 90% null):")
-print(flags)
-
-# Numeric distribution summaries
-ClosePriceSummary = sold["ClosePrice"].describe()
-LivingAreaSummary = sold["LivingArea"].describe()
-DaysOnMarketSummary = sold["DaysOnMarket"].describe()
-print("Close Price Numeric Distribution Summary:", ClosePriceSummary)
-print("Living Area Numeric Distribution Summary:", LivingAreaSummary)
-print("Days On Market Numeric Distribution Summary:", DaysOnMarketSummary)
-
-# Create a copy
-mainsold = sold.copy()
-
-# Export as sold CSV
-# mainsold.to_csv('/Users/eshaanchachad/Desktop/IDXExchange/CRMLSSold2.csv', index=False)
-
-# Import CSV
-listing = pd.read_csv("/Users/eshaanchachad/Desktop/IDXExchange/CRMLSListing.csv")
-listing = listing[listing["PropertyType"] == "Residential"]
-
-# print("Listing Report")
-
-nullcount = listing.isnull().sum()
-print("Count of null values")
-print(nullcount)
-
-#Print column types
-types = listing.dtypes
-print("Column types")
-print(types)
-
-# Percentage of null columns
-nullcolumns = listing.isnull().mean()*100
-print("Percent of null values within columns:")
-print(nullcolumns)
-
-# Flag columns with null values over 90%
-flaggedcols = listing.isnull().mean() >= 0.9
-flags = listing.columns[flaggedcols]
-print("Flagged columns (over 90% null):")
-print(flags)
-
-# Numeric distribution summaries
-ClosePriceSummary = listing["ClosePrice"].describe()
-LivingAreaSummary = listing["LivingArea"].describe()
-DaysOnMarketSummary = listing["DaysOnMarket"].describe()
-print("Close Price Numeric Distribution Summary:", ClosePriceSummary)
-print("Living Area Numeric Distribution Summary:", LivingAreaSummary)
-print("Days On Market Numeric Distribution Summary:", DaysOnMarketSummary)
-
-# Create a copy
-mainlisting = listing.copy()
-
-# Export as listing CSV
-# mainlisting.to_csv('/Users/eshaanchachad/Desktop/IDXExchange/CRMLSListing2.csv', index=False)
-
-
-
-#Histograms:
-
+import numpy as np
 import matplotlib.pyplot as plt
+
+# Read CSVs
+sold = pd.read_csv("/Users/eshaanchachad/Desktop/IDXExchange/csvs/CRMLSSold.csv")
+listing = pd.read_csv("/Users/eshaanchachad/Desktop/IDXExchange/csvs/CRMLSListing.csv")
+
+
+
+# Create environments
+
+# Create an environment for numeric distribution summaries
+
+def summary(df, column):
+    summary = df[column].describe()
+    return summary
+
+# Environment to create a histogram
 
 def histogram(df, column):
     Q1 = df[column].quantile(0.25)
@@ -102,7 +34,9 @@ def histogram(df, column):
     plt.ylabel("Frequency")
     plt.show()
 
-# Boxplot:
+    # Environment to create a boxplot
+
+# Environment to create a boxplot
 
 def boxplot(df, column):
     Q1 = df[column].quantile(0.25)
@@ -119,19 +53,7 @@ def boxplot(df, column):
     plt.ylabel(column)
     plt.show()
 
-
-# boxplot(sold, "ClosePrice")
-
-# Percentile summary
-
-def percentile_sumamry(df, column):
-    summary = df[column].describe()
-    return summary
-
-# print(percentile_sumamry(sold, "ClosePrice"))
-
-
-# Extreme outliers
+# Environment to identify extreme outliers
 
 def extreme_outliers(df, column):
     EEQ1 = df[column].quantile(0.25)
@@ -149,4 +71,107 @@ def extreme_outliers(df, column):
     extreme_outliers = extreme_data[column]
     return(extreme_outliers)
 
-# print(extreme_outliers(listing, "ClosePrice"))
+
+
+
+
+# Sold analysis
+
+# Dataset understanding
+
+# Identify number of rows and columns
+rows = len(sold)
+columns = len(sold.columns)
+
+print(rows,"rows in Sold")
+print(columns,"columnsin Sold")
+
+# Identify column data types
+print("Sold Data Types")
+print(sold.dtypes)
+
+# Print null percent for all columns
+sold_null_percent = sold.isnull().mean()
+print("Sold Percent of null values per columns")
+print(sold_null_percent * 100)
+
+#  Identify columns with 90% or more null values
+sold_columns_90 = sold_null_percent[sold_null_percent >= 0.9].index
+
+print("Sold Columns with 90%+ missing values")
+print(sold_columns_90)
+
+# Remove columns
+
+# Remove columns with 90% or higher null
+sold = sold.drop(columns=sold_columns_90)
+lenafter90 = len(sold.columns)
+print(lenafter90,"columns after removing 90% null Sold columns")
+
+# Numeric distribution summaries for columns
+
+print("Sold Numeric Distribution Summary for ClosePrice:")
+print(summary(sold,"ClosePrice"))
+
+print("Sold Numeric Distribution Summary for LivingArea:")
+print(summary(sold,"LivingArea"))
+
+print("Sold Numeric Distribution Summary for DaysOnMarket:")
+print(summary(sold,"DaysOnMarket"))
+
+
+
+
+
+
+
+
+# Listing analysis
+
+# Dataset understanding
+
+# Identify number of rows and columns
+rows = len(listing)
+columns = len(listing.columns)
+
+print(rows,"rows in Listing")
+print(columns,"columns in Listing")
+
+# Identify column data types
+print("Listing Data Types")
+print(listing.dtypes)
+
+# Print null percent for all columns
+listing_null_percent = listing.isnull().mean()
+print("Listing Percent of null values per columns")
+print(listing_null_percent * 100)
+
+#  Identify columns with 90% or more null values
+listing_columns_90 = listing_null_percent[listing_null_percent >= 0.9].index
+
+print("Listing Columns with 90%+ missing values")
+print(listing_columns_90)
+
+# Remove columns
+
+# Remove columns with 90% or higher null
+listing = listing.drop(columns=listing_columns_90)
+lenafter90 = len(listing.columns)
+print(lenafter90,"columns after removing 90% null Listing columns")
+
+# Numeric distribution summaries for columns
+
+print("Listing Numeric Distribution Summary for ClosePrice:")
+print(summary(listing,"ClosePrice"))
+
+print("Listing Numeric Distribution Summary for LivingArea:")
+print(summary(listing,"LivingArea"))
+
+print("Listing Numeric Distribution Summary for DaysOnMarket:")
+print(summary(listing,"DaysOnMarket"))
+
+soldmain = sold.copy()
+listingmain = listing.copy()
+
+soldmain.to_csv('/Users/eshaanchachad/Desktop/IDXExchange/csvs/CRMLSSold2.csv', index=False)
+listingmain.to_csv('/Users/eshaanchachad/Desktop/IDXExchange/csvs/CRMLSListing2.csv', index=False)
